366f342d0500b06ba5a54351bb21b80eb7409260
MusicHero <andrey.obodov011@mail.ru>
2021-09-14 21:35:14 +0500

    фиксация: Changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ interface arr{
 
 export const ar=[
   {id:1, name:'Andrey', imgsrc:'https://mdate.zahav.ru/media/images/polls/748/tb_poll.jpg', lastmsg:'Something'},
-  {id:2, name:'Daniil', imgsrc:'https://zabaka.ru/upload/2016/02/celebrity/celebrity_07.jpg', lastmsg:'that'},
+  {id:2, name:'Danil', imgsrc:'https://zabaka.ru/upload/2016/02/celebrity/celebrity_07.jpg', lastmsg:'that'},
   {id:3, name:'Vlad', imgsrc:'http://ru.fishki.net/picsw/082011/18/post/portret/portret-006.jpg', lastmsg:'we '},
   {id:4, name:'Vladimir', imgsrc:'http://www.weareart.ru/public/uploads/news/967/b-IMG_2128edfef1b9.jpg', lastmsg:'accept'}
 ]
