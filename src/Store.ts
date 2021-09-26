4f14e3d286667622199ca0d30533729cff04b57d
Ceratonin <alkash411@gmail.com>
2021-09-26 20:22:53 +0500

    Store v.1.2

--- src/Store.ts
+++ src/Store.ts
@@ -1,5 +1,32 @@
-import { Subject } from "rxjs"
+import { Subject } from "rxjs";
 
+const subject = new Subject();
 export class Store<T> {
-    componentDidmount
+  state:T = this.state;
+  initialState: T;
+
+  init() {
+    new Store<T>();
+  }
+
+  componentWillUnmount() {
+    subject.unsubscribe();
+  }
+
+  setStore(setSub: T) {
+    subject.subscribe(setSub);
+  }
+
+  pushToStore(data: T) {
+    this.state = {
+      ...this.state,
+      data: [...this.state, data],
+    };
+    subject.next(this.state);
+  }
+
+  resetStore() {
+      this.state = initialState;
+      subject.next(this.state);
+    };
 }
